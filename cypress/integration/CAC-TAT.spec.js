4bd4af9f98d59dc7df8cc9aefe0287db5b3dd60b
Lion Simon <lionsimongondim@gmail.com>
2023-01-26 12:37:47 -0300

    Acessando o html e validando o título da página

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe("Central de Atendimento ao Cliente TAT", () => {
+  it("verifica o título da aplicação", () => {
+    cy.visit("./src/index.html");
+    cy.title().should("eq", "Central de Atendimento ao Cliente TAT");
+  });
+});
