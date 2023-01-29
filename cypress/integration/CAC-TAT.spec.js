8d648e764e2e27714f77122f3fc35d0796fdad9f
Lion Simon <lionsimongondim@gmail.com>
2023-01-29 18:43:06 -0300

    Selecionando opções em campos de seleção suspensa

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -72,4 +72,14 @@ describe("Central de Atendimento ao Cliente TAT", () => {
   it("envia o formuário com sucesso usando um comando customizado", () => {
     cy.fillMandatoryFieldsAndSubmit();
   });
+
+  it("seleciona um produto (YouTube) por seu texto", () => {
+    cy.get("#product").select("YouTube").should("have.value", "youtube");
+  });
+  it("seleciona um produto (Mentoria) por seu valor (value)", () => {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
+  });
+  it("seleciona um produto (Blog) por seu índice", () => {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
 });
