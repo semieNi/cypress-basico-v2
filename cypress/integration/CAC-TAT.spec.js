bd6b936242f272cc883a0cef0df9dce046b2071f
Lion Simon <lionsimongondim@gmail.com>
2023-02-04 01:03:10 -0300

    Fazendo upload de arquivos com Cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -95,12 +95,36 @@ describe("Central de Atendimento ao Cliente TAT", () => {
         cy.wrap($radio).should("be.checked");
       });
   });
-  it.only("marca ambos checkboxes, depois desmarca o último", () => {
+  it("marca ambos checkboxes, depois desmarca o último", () => {
     cy.get('input[type="checkbox"]')
       .check()
       .should("be.checked")
       .last()
       .uncheck()
       .should("not.be.checked");
+  });
+  it("selecina um arquivo da pasta fixtures", () => {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
+      .selectFile("cypress/fixtures/example.json")
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+  it("seleciona um arquivo simulando um drag-and-drop", () => {
+    cy.get('input[type="file"]#file-upload')
+      .should("not.have.value")
+      .selectFile("cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get('input[type="file"]')
+      .selectFile("@sampleFile")
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
   });
 });
