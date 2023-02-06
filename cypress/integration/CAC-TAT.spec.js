d9884089d986fd196f7ba21d1129bf9016277c83
Lion Simon <lionsimongondim@gmail.com>
2023-02-06 17:37:04 -0300

    Usando o comando Cypress._.repeat( ) and invoke

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -144,4 +144,27 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get("#privacy a").invoke("removeAttr", "target").click();
     cy.contains("Talking About Testing").should("be.visible");
   });
+  it.only("exibe e esconde as mensagens de sucesso e erro usando o .invoke", () => {
+    cy.get(".success")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Mensagem enviada com sucesso.")
+      .invoke("hide")
+      .should("not.be.visible");
+    cy.get(".error")
+      .should("not.be.visible")
+      .invoke("show")
+      .should("be.visible")
+      .and("contain", "Valide os campos obrigatórios!")
+      .invoke("hide")
+      .should("not.be.visible");
+  });
+  it.only("preenche a area de texto usando o comando invoke", () => {
+    const longText = Cypress._.repeat("0123456789", 20);
+
+    cy.get("#open-text-area")
+      .invoke("val", longText)
+      .should("have.value", longText);
+  });
 });
